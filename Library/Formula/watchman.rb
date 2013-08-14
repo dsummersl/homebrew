49081c4afc72db4133434de86c0424c33be92a5f
Dane Summers <dsummersl@yahoo.com>
2013-08-14 08:01:56 -0400

    watchman 2.8.2
    Issue #21611

--- Library/Formula/watchman.rb
+++ Library/Formula/watchman.rb
@@ -2,29 +2,36 @@ require 'formula'
 
 class Watchman < Formula
   homepage 'https://github.com/facebook/watchman'
-  head 'https://github.com/facebook/watchman.git'
   url 'https://github.com/facebook/watchman/archive/v2.8.2.tar.gz'
   sha1 'd46f5fa29de4fb30d7dcc0ec7998099567b09a5f'
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
+  head 'https://github.com/facebook/watchman.git'
+
+  depends_on :autoconf => :build
+  depends_on :automake => :build
   depends_on 'pkg-config' => :build
   depends_on 'pcre' => :recommended
 
   def install
+    args = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}"
+    ]
+
     system "./autogen.sh"
+
     if build.with? 'pcre'
-      system "./configure", "--disable-debug", "--disable-dependency-tracking", "--with-pcre", "--prefix=#{prefix}"
+      system "./configure", "--with-pcre", *args
     else
-      system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}"
+      system "./configure", *args
     end
+
     system "make"
     system "make install"
   end
 
   def caveats
-    # display OSX specific notes about file handle size limits:
     <<-EOS.undent
     To increase file limits add 'kern.maxfiles=10485760' and 'kern.maxfilesperproc=10485760'
     to /etc/sysctl.conf (use 'sysctl -w' to do so immediately).
